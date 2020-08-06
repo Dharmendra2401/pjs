099a1c64076665438d88fe62c2a92320fc31c35e
VTS01 <VTS01@github>
2020-08-06 18:19:50 +0530

    profile,css,saved profile,modal.hmtl

---
+++ profile.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap4/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome5/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-3">
+    			<img class="" width="110" src="images/flooop.png">
+    		</div>
+    		<div class="col-md-6 d-flex justify-content-center">
+    			<div class="input-group my-auto">
+				  <div class="input-group-prepend">
+				    <span class="input-group-text"><i class="fa fa-search"></i></span>
+				  </div>
+				  <input type="text" class="form-control" placeholder="Search" aria-label="Username">
+				</div>
+    		</div>
+    		<div class="col-md-3 align-self-center text-right">
+				 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginPopup">
+				    LOGIN/SIGUP
+				  </button>
+    		</div>
+    		<div class="col-md-12">
+				<nav class="navbar navbar-expand-sm">
+				  <ul class="navbar-nav">
+				    <li class="nav-item">
+				      <a class="nav-link" href="#">About Us</a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link" href="#">Events</a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link" href="#">Gallery</a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link" href="#">Schemes</a>
+				    </li>
+				    <li class="nav-item">
+				      <a class="nav-link" href="#">Zones</a>
+				    </li>
+				  </ul>
+				</nav>
+		    </div>
+		    <div class="col-md-2 user-tab">
+		    	<ul class="nav nav-tabs">
+				    <li class="nav-item"><a class="nav-link active" href="#overview">Overview</a></li>
+				    <li class="nav-item"><a class="nav-link" href="#basic-info">Basic Info</a></li>
+				    <li class="nav-item"><a class="nav-link" href="#contact">Contact & Address Info</a></li>
+				    <li class="nav-item"><a class="nav-link" href="#education">Education & Work</a></li>
+				</ul>
+			</div>
+			<div class="col-md-10 tab-content user-profile">	
+                <div id="overview" class="row tab-pane active">
+                	<div class="col-md-12 tab">
+                		<h3>Personal Info</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Full Name <strong>:</strong></div>
+                			<div class="col-md-9">lavish lokendra jain</div>
+
+                			<div class="col-md-3">Popular Name<strong>:</strong></div>
+                			<div class="col-md-9">lavish</div>
+
+                			<div class="col-md-3">Gender <strong>:</strong></div>
+                			<div class="col-md-9">male</div>
+
+                			<div class="col-md-3">Status <strong>:</strong></div>
+                			<div class="col-md-9">single</div>
+
+                			<div class="col-md-3">Blood Group<strong>:</strong></div>
+                			<div class="col-md-9">b+</div>
+
+                			<div class="col-md-3">Height <strong>:</strong></div>
+                			<div class="col-md-9">5'9"</div>
+                		</div>
+                		<h3>Birth Details</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Date of Birth <strong>:</strong></div>
+                			<div class="col-md-9">lavish lokendra jain</div>
+
+                			<div class="col-md-3">Birth Time<strong>:</strong></div>
+                			<div class="col-md-9">lavish</div>
+
+                			<div class="col-md-3">Birth Place <strong>:</strong></div>
+                			<div class="col-md-9">male</div>
+                		</div>
+                		<h3>Contact Info</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Mobile No. <strong>:</strong></div>
+                			<div class="col-md-9">1234567890</div>
+
+                			<div class="col-md-3">Email Id<strong>:</strong></div>
+                			<div class="col-md-9">lavish@gmail.com</div>
+                		</div>
+                		<h3>Address Info</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Address <strong>:</strong></div>
+                			<div class="col-md-9">
+                				<p>J-11 DHANVANTRI APPARTMENT M.O.G LINE,</p>
+                				<p>BALSAMAD, MADHYA PRADESH, INDIA</p>
+                				<p>452001</p>
+                            </div>
+                		</div>
+                		<h3>Education</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Education <strong>:</strong></div>
+                			<div class="col-md-9">bachelor of engineering</div>
+                		</div>
+                		<h3>Work</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Occupation <strong>:</strong></div>
+                			<div class="col-md-9">job/software engineer</div>
+
+                			<div class="col-md-3">Income<strong>:</strong></div>
+                			<div class="col-md-9">1-2 lakh</div>
+                		</div>
+                	</div>
+                </div>
+                <div id="basic-info" class="row tab-pane">
+                	<div class="col-md-12 tab">
+                		<h3>Personal Info</h3>
+                		<div class="row info">
+                			<div class="col-md-3">First Name <strong>:</strong></div>
+                			<div class="col-md-9">lavish</div>
+
+                			<div class="col-md-3">Middle Name<strong>:</strong></div>
+                			<div class="col-md-9">lavish</div>
+
+                			<div class="col-md-3">Last Name <strong>:</strong></div>
+                			<div class="col-md-9">male</div>
+
+                			<div class="col-md-3">Popular Name <strong>:</strong></div>
+                			<div class="col-md-9">single</div>
+
+                			<div class="col-md-3">Gender<strong>:</strong></div>
+                			<div class="col-md-9">b+</div>
+
+                			<div class="col-md-3">Status <strong>:</strong></div>
+                			<div class="col-md-9">5'9"</div>
+
+                			<div class="col-md-3">Blood Group<strong>:</strong></div>
+                			<div class="col-md-9">b+</div>
+
+                			<div class="col-md-3">Height <strong>:</strong></div>
+                			<div class="col-md-9">5'9"</div>
+                		</div>
+                		<h3>Birth Details</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Date of Birth <strong>:</strong></div>
+                			<div class="col-md-9">01/01/01</div>
+
+                			<div class="col-md-3">Birth Time<strong>:</strong></div>
+                			<div class="col-md-9">2:00 am</div>
+
+                			<div class="col-md-3">Birth Place <strong>:</strong></div>
+                			<div class="col-md-9">maheshwar</div>
+                		</div>
+                	
+                		
+                		
+                	</div>
+                </div>
+			    <div id="contact"  class="row tab-pane">
+			    	<h3>Contact Info</h3>
+            		<div class="row info">
+            			<div class="col-md-3">Mobile No. <strong>:</strong></div>
+            			<div class="col-md-9">1234567890</div>
+
+            			<div class="col-md-3">Email Id<strong>:</strong></div>
+            			<div class="col-md-9">lavish@gmail.com</div>
+            		</div>
+            		<h3>Address Info</h3>
+            		<div class="row info">
+            			<div class="col-md-3">Address  <strong>:</strong></div>
+            			<div class="col-md-9">J-11 DHANVANTRI APPARTMENT M.O.G LINE</div>
+
+            			<div class="col-md-3">Name of city/town/village <strong>:</strong></div>
+            			<div class="col-md-9">indore</div>
+
+            			<div class="col-md-3">Pin Code  <strong>:</strong></div>
+            			<div class="col-md-9">452001</div>
+
+            			<div class="col-md-3">State <strong>:</strong></div>
+            			<div class="col-md-9">madya pradesh</div>
+
+            			<div class="col-md-3">Country <strong>:</strong></div>
+            			<div class="col-md-9">India</div>
+            		</div>
+			    </div>
+			    <div id="education"  class="row tab-pane">
+			    	<h3>Education</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Education <strong>:</strong></div>
+                			<div class="col-md-9">bachelor of engineering</div>
+                		</div>
+                		<h3>Work</h3>
+                		<div class="row info">
+                			<div class="col-md-3">Occupation <strong>:</strong></div>
+                			<div class="col-md-9">job/software engineer</div>
+
+                			<div class="col-md-3">Income<strong>:</strong></div>
+                			<div class="col-md-9">1-2 lakh</div>
+                		</div>
+			    </div>
+		    </div>
+		</div>	
+    	</div>
+    </div>
+</body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap4/dist/js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function(){
+  $(".nav-tabs a").click(function(){
+    $(this).tab('show');
+  });
+  $('.nav-tabs a').on('shown.bs.tab', function(event){
+    var x = $(event.target).text();         // active tab
+    var y = $(event.relatedTarget).text();  // previous tab
+    $(".act span").text(x);
+    $(".prev span").text(y);
+  });
+});
+</script>
+
+</html>
