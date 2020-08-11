8fa8f33713ef1e113d8fce47abab4d64f3471ade
120harsh <66354500+120harsh@users.noreply.github.com>
2020-08-11 15:00:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	 <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-	<title> vts technology</title>
+	<title> vts not avalable</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap4/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fontawesome5/css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
