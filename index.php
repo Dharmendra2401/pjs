48681e90e57c5c185f76591a13459bec8f58987a
120harsh <shuklaharsh989@gmail.com>
2020-09-10 16:37:31 +0530

    harsh

--- index.php
+++ index.php
@@ -1,64 +1,6 @@
-
 <?php
 include "config/config.php";
-<<<<<<< Updated upstream
-=======
-redirect(RE_EN_PATH."index.php")
-?>
-=======
-	<title>vts dfggdfgfdgdf is ther </title>
-
-	<link rel="stylesheet" type="text/css" href="bootstrap4/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="fontawesome5/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-    <div class="container-fluid">
-    	  <div class="row">
-    		<div class="col-md-3">
-    			<img class="" width="110" src="images/flooop.png">
-    		</div>
-    		<div class="col-md-6 d-flex justify-content-center">
-    			<div class="input-group my-auto">
-				  <div class="input-group-prepend">
-				    <span class="input-group-text"><i class="fa fa-search"></i></span>
-				  </div>
-				  <!-- <input type="text" class="form-control" placeholder="Search" aria-label="Username"> -->
-				  <input type="text" class="form-control" placeholder="Search" size="30" onkeyup="showResult(this.value)">
-                  <div id="livesearch"></div>
-				</div>
-    		</div>
-    		<div class="col-md-3 align-self-center text-right">
-				 <button type="button" class="btn btn-primary open-login" data-toggle="modal" data-target="#loginPopup">
-				    LOGIN/SIGUP
-				  </button>
-    		</div>
-    		<div class="col-md-12 mb-2">
-    			<nav class="navbar navbar-expand-sm nav-bg">
-				  <ul class="navbar-nav">
-				    <li class="nav-item">
-				      <a class="nav-link" href="#">About Us</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" href="#">Events</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" href="#">Gallery</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" href="#">Schemes</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" href="#">Zones</a>
-				    </li>
-				  </ul>
-				</nav>
-    		</div>
-		  </div>
->>>>>>> Stashed changes
-
 redirect(RE_EN_PATH."index.php"); 
-
 ?>
   
 
