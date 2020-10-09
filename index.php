53c0953a917d0f732453275bcb21157ee9b6209d
120harsh <shuklaharsh989@gmail.com>
2020-10-09 16:18:01 +0530

    harsh 09-10-2020

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 
+<<<<<<< Updated upstream
 <?php
 include "config/config.php";
 redirect(RE_EN_PATH."index.php")
 ?>
+=======
+	<title>vts dfggdfgfdgdf is ther </title>
+>>>>>>> Stashed changes
 
 	
