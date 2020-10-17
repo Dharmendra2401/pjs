3fc9625024326d4e34007fabb586db91ae492b22
saurav singh <saurav@vihatechnosoft.com>
2020-10-17 11:59:20 +0530

    Revert "remove edit father"
    This reverts commit 46acc4760e9b655d865cd6bd9a6ade582f303e17.

--- en/PJS-demo/send_request.php
+++ en/PJS-demo/send_request.php
@@ -35,7 +35,7 @@ $checkcurrent_relation_query = mysqli_query($con,$checkcureent_relation)or die(m
 $data=mysqli_fetch_assoc($checkcurrent_relation_query);
 if (mysqli_num_rows($checkcurrent_relation_query)>0 && $data['count']!='Null') {
 	if ($data['count']=='from') {
-		echo "You have already sent request";
+		echo "you already sent request to someone";
 	}
 	if ($data['count']=='to') {
 		echo "someone already sent you request";
