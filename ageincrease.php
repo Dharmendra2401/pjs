02211089708c0091de34c9a0b052e327fef1aeb0
120harsh <shuklaharsh989@gmail.com>
2020-10-20 15:11:28 +0530

    harsh updated 20-10-2020

---
+++ ageincrease.php
@@ -0,0 +1,17 @@
+<?php
+include 'config/config.php';
+$getdate=date('d-m-Y');
+
+$query=mysqli_query($con,'select date_of_birth,Life_status,member_id from member where Life_status="L" and date_of_birth LIKE "%'.date('m-d').'%" ');
+while($fetchdate=mysqli_fetch_array($query)){
+$getadte= date('d-m',strtotime($fetchdate['date_of_birth'])).'<br>';
+$currentdate=date('d-m'); 
+if($getadte==$currentdate){
+$getyear= date('Y',strtotime($fetchdate['date_of_birth'])).'<br>';
+$getage=date('Y')-$getyear;
+mysqli_query($con,'update member set age="'.$getage.'" where member_id="'.$fetchdate['member_id'].'" ');
+}
+
+}
+
+?>
