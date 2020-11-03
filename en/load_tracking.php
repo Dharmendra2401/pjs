36ea7d26e057ff3c100c9c85014a431423ac10e9
120harsh <shuklaharsh989@gmail.com>
2020-11-03 16:35:28 +0530

    harsh add 11/03/2020

---
+++ en/load_tracking.php
@@ -0,0 +1,66 @@
+
+<?php
+include "../config/config.php";
+$getrequest=$_REQUEST['status'];
+$query=mysqli_query($con,'select request_id,active_status,record_inserted_dttm,record_updated_dttm from staging_approval where request_id="'.$getrequest.'" ');
+$countt=mysqli_num_rows($query);
+if($countt>0){
+$row=mysqli_fetch_array($query);
+?>
+
+<div class="row top-tracker-icon">
+<div class="col-md-2 text-center"></div>
+<div class="col-md-2 text-center margin-manage"><div class="icon-tracking text-success"><i class="fas fa-arrow-circle-right"></i><h6>Submited</h6> <small class="px-md-2 alert-secondary alert-sm"><?php echo date('d/m/Y',strtotime($row['record_inserted_dttm'])); ?> </small>  </div><h6 class="border tracking-border"></h6></div>
+<div class="col-md-2 text-center margin-manage">
+
+<?php if($row['active_status']=='Y') {?>
+
+<div class="icon-tracking text-secondary">
+<i class="fas fa-arrow-circle-right"></i><h6>Wating for Approval</h6></div>
+
+<?php }  else if($row['active_status']=='U') {?> 
+<div class="icon-tracking text-warning"> 
+<i class="fas fa-times-circle"></i> 
+<h6>Updated</h6> <small class="px-md-2 alert-secondary alert-sm"><?php echo date('d/m/Y',strtotime($row['record_updated_dttm'])); ?> </small> </div>
+
+
+ <?php } else if($row['active_status']=='R') {?><?php echo $row['active_status']; ?>  
+<div class="icon-tracking text-danger"> 
+<i class="fas fa-times-circle"></i> 
+<h6>Rejected</h6> <small class="px-md-2 alert-secondary alert-sm"><a href="<?php echo RE_EN_PATH;?>userupdate/update.php?token=<?php echo base64_encode($row['request_id']);?>" style="text-decoration: underline;">Click here </a> to upate your form</a> </small></div>
+<?php } else { ?>
+ 
+<div class="icon-tracking text-success"> 
+<i class="fas fa-arrow-circle-right"></i> 
+<h6>Approved</h6></div>
+
+<?php } ?>
+<h6 class="border tracking-border"></h6>  
+</div>
+
+
+<div class="col-md-2 text-center margin-manage">
+<?php if($row['active_status']=='N') {?>
+<div class="icon-tracking text-success"><i class="fas fa-arrow-circle-right"></i><h6>Member Id Assigned</h6></div>
+<?php } else{?>
+<div class="icon-tracking text-secondary"><i class="fas fa-arrow-circle-right"></i><h6>Member Id Not Assigned</h6></div>
+<?php } ?>
+<h6 class="border tracking-border"></h6></div>
+
+
+
+<div class="col-md-2 text-center margin-manage">
+<?php if($row['active_status']=='N') {?>
+<div class="icon-tracking text-success"><i class="fas fa-check-circle"style="transform: initial;"></i><h6>Process Completed</h6></div>
+<div class="col-md-2 text-center"></div>
+<?php }else {?>
+<div class="icon-tracking text-secondary"><i class="fas fa-times-circle" ></i><h6>Process Incomplete</h6></div>
+<div class="col-md-2 text-center"></div>
+<?php } ?>
+
+</div>     
+</div><br>
+</div>
+<?php }else {?>
+<div class="text-center"><span class="alert-danger" style="padding: 5px;border-radius: 5px;">Sorry!No Records Found</span></div><br>
+<?php } ?>
---
+++ en/tracking.php
@@ -0,0 +1,84 @@
+
+<?php  
+include "../config/config.php";
+
+?>
+<!DOCTYPE html>
+<html>
+<?php include "../styles.php";  ?>
+<body>
+<div class="container-fluid">
+
+<?php include "header.php";  ?>
+</div>
+<style>.icon-tracking i{    font-size: 40px;
+} .form-tracker{margin-top:50px; margin-bottom:50px;} .tracking-border{position: absolute;
+/* left: 151px; */
+width: 92px;
+right: -42px;
+top: 24px;}
+
+
+@media screen and (max-width: 998px) {
+.tracking-border {
+    position: absolute;
+    /* left: 151px; */
+    width: 67px;
+    right: -32px;
+    top: 20px;
+}
+}
+
+@media screen and (max-width: 768px) {
+.icon-tracking i {
+    font-size: 28px;
+    transform: rotate(88deg);
+}
+.margin-manage{margin-bottom: -7px;
+    margin-top: -1px;}
+.tracking-border {
+    /* position: absolute; */
+    /* left: 151px; */
+    width: 0px;
+    height: 50px;
+    right: -32px;
+    top: 20px;
+    display: inline-block;
+    position: initial;
+
+}
+}
+</style>
+<div class="shadow">
+<div class="row">
+<form class="form-inline col-md-6 offset-4 form-tracker">
+<div class="form-group mx-sm-3 mb-2">
+<input type="text" class="form-control text-uppercase" id="status" placeholder="Enter refrence id">
+</div>
+<button type="button" onclick="return getStatus();" class="btn btn-primary mb-2">Track Status</button>
+</form>
+</div>
+<span id="trackstatus"></span>
+<?php include "../footer.php" ?>
+</body>
+
+<?php include "../script.php" ?>
+
+<script>
+function getStatus(){
+var status=$('#status').val();
+$('#loadergif').fadeIn();
+$.ajax({
+method:'POST',
+url:'<?php echo RE_EN_PATH; ?>load_tracking.php',
+data:{'status':status},
+success:function(gettracking){
+$('#trackstatus').html(gettracking);
+$('#loadergif').fadeOut();
+}
+
+})
+
+}
+</script>
+</html>
